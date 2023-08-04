23a7145c855c05a74e9bff4072abb242700d5c23
Gi-thinji <githinji.ngare@gmail.com>
2023-08-04 12:55:37 +0300

    Done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>{/*{code here}*/}
+  <h1>Movies Page</h1>
+  {movies.map((movie)=>{
+    return (
+    <div key={movie.title}>
+      {movie.title}
+      <br></br>
+      {movie.time} minutes
+      <ul>
+        {movie.genres.map((genre)=>{
+          return <li key={genre}>{genre}</li>
+        })}
+      </ul>
+    </div>
+    )
+
+  })}
+  </div>;
 }
 
 export default Movies;
